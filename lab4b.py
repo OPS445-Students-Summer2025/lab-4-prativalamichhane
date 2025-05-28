05a761a2ea2809284c91deabb269e026f465ab2f
Prativa Lamichhane <plamichhane2@myseneca.ca>
2025-05-28 11:36:49 -0400

    Updated spacing lab4b

--- lab4b.py
+++ lab4b.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
+
 def join_lists(l1, l2):
     # join_lists will return a list that contains every value from both l1 and l2
      return list(set(l1) | set(l2)) 
+
 def match_lists(l1, l2):
     # match_lists will return a list that contains all values found in both l1 and l2
     return list(set(l1) & set(l2))
+
 def diff_lists(l1, l2):
     # diff_lists will return a list that contains all different values, which are not shared between the lists
    return list(set(l1) ^ set(l2))
+
 if __name__ == '__main__':
     list1 = list(range(1,10))
     list2 = list(range(5,15))
