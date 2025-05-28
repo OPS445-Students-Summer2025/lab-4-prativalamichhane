6b0f653e005c2837dbbaaf2ffbb818b2d9afdcae
Prativa Lamichhane <plamichhane2@myseneca.ca>
2025-05-28 11:32:49 -0400

    Updated spacing lab4d

--- lab4d.py
+++ lab4d.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 # Strings 1
+
 str1 = 'Hello World!!'
 str2 = 'Seneca College'
+
 num1 = 1500
 num2 = 1.50
+
 def first_five(string):
     # Return first five characters of the string
     return string[:5]
+
 def last_seven(string):
     # Return last seven characters of the string
     return string[-7:]
+
 def middle_number(num):
     # Return middle part of the number as a string slice
     num_str = str(num)  # Convert the number to a string
     return num_str[1:3]
+
 def first_three_last_three(string1, string2):
     # Return first three characters of string1 + last three characters of string2
     return string1[:3] + string2[-3:]
+
 if __name__ == '__main__':
     print(first_five(str1))
     print(first_five(str2))
